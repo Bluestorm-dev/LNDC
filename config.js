f94647a9864d2c25ea03ba760ba7d5bbcd46d6f2
Bluestorm-dev <yoann.bluestorm@proton.me>
2026-08-27 16:08:26 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.NIDC_CONFIG = {
-  SUPABASE_URL: "https://YOUR_PROJECT.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY",
+  SUPABASE_URL: "https://nwmewsenpaxvxzbjsair.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_hKRN04bCtid5wliSMQfsPg_BtBKLu8X",
   APP_VERSION: "0.9.8",
   DEFAULT_SEASON_SLUG: "ucl-2026-27",
   ODDS_EXTERNAL_ENABLED: false,
